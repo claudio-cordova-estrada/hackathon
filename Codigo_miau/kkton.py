5bac2d594a229678838ded2731635db72fc620a2
claudio-cordova-estrada <claudionicolascordova@gmail.com>
2022-06-11 02:41:48 -0400

    antes de comitear a lo rapido

--- Codigo_miau/kkton.py
+++ Codigo_miau/kkton.py
@@ -21,17 +21,9 @@ print(hora)
 print(loc)"""
 
 
-"""
-# usar loc para las cordenadas, hora para la fecha y hora(SON LOS PARAMETROS DEL INSERT)
-arduino = serial.Serial("COM4", 9600)"""
-
 """def insert_sos(p_cod, p_ubicacion, p_rut, p_fecha):
     sql = ('insert into SOS(p_cod, p_ubicacion, p_rut, p_fecha)'
             'values(:cod, :ubicacion, :rut, :fecha)')"""
-    
-    
 
-"""while True:
-    switch = arduino.readline().decode("ascii")
-    if switch == "1":
-        print("XD")"""
+       
+        
